f01a3a388d7f445ffb2dba3628c8335b0c870b98
Andrii Puhachenko <andrqxa@gmail.com>
2019-07-01 23:07:48 +0300

    Fixed parseFullName function

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -2,9 +2,13 @@ package ru.skillbranch.devintensive.utils
 
 object Utils {
     fun parseFullName(fullName: String?): Pair<String?, String?> {
-        val parts: List<String>? = fullName?.split(" ")
-        val firstName = parts?.getOrNull(0)
-        val lastName = parts?.getOrNull(1)
+        val regex = Regex("""\s*""")
+        if (fullName.isNullOrEmpty() || regex.matches(fullName)) {
+            return null to null
+        }
+        val parts: List<String> = fullName.split(" ")
+        val firstName = parts.getOrNull(0)
+        val lastName = parts.getOrNull(1)
         return firstName to lastName
     }
 }
