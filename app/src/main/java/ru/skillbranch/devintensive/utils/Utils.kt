69485c8ad83aaca63b4d1e231614cefff7e6e2aa
Andrii Puhachenko <andrqxa@gmail.com>
2019-07-03 23:54:02 +0300

    Added skeleton of additional extensions functions

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -13,4 +13,12 @@ object Utils {
         val lastName = parts.getOrNull(1)
         return firstName to lastName
     }
+
+    fun transliteration(payload: String, divider: String = " "): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    fun toInitials(firstName: String?, lastName: String?): String? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
