9159d7ea6a307647a91bac2a0d01f3a53fcbde9b
Andrii Puhachenko <andrqxa@gmail.com>
2019-07-03 22:08:27 +0300

    Added Date.add function

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,10 +1,33 @@
 package ru.skillbranch.devintensive.extensions
 
 
+import java.lang.IllegalArgumentException
 import java.text.SimpleDateFormat
 import java.util.*
+
+const val SECOND = 1000L
+const val MINUTE = 60 * SECOND
+const val HOUR = 60 * MINUTE
+const val DAY = 24 * HOUR
+const val WEEK = 7 * DAY
+const val DECADE = 10 * DAY
+
 
 fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy", locale : String = "ru"): String {
     val dateFormat = SimpleDateFormat(pattern, Locale(locale))
     return dateFormat.format(this)
+}
+
+fun Date.add(value : Int, inputUnits : String) : Date{
+    val units = inputUnits.toLowerCase()
+    this.time += when(units){
+        "second", "seconds", "sec", "secs" -> value * SECOND
+        "minute", "minutes", "min", "mins" -> value * MINUTE
+        "hour", "hours", "hr", "hrs" -> value * HOUR
+        "day", "days" -> value * DAY
+        "week", "weeks" -> value * WEEK
+        "decade", "decades" -> value * DECADE
+        else -> throw IllegalArgumentException("invalid unit")
+    }
+    return this
 }
--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,8 +1,10 @@
 package ru.skillbranch.devintensive.utils
 
 object Utils {
-    fun parseFullName(fullName: String?): Pair<String?, String?> {
+    fun parseFullName(inputFullName: String?): Pair<String?, String?> {
         val regex = Regex("""\s*""")
+        val name1 = inputFullName?.replace("\\s+".toRegex(), " ")
+        val fullName = name1?.trim()
         if (fullName.isNullOrEmpty() || regex.matches(fullName)) {
             return null to null
         }
