9f66a0ddebd27fb2399ec2d4b69ea7924c6b3d2d
Andrii Puhachenko <andrqxa@gmail.com>
2019-07-05 00:55:00 +0300

    Added Messages

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
@@ -0,0 +1,31 @@
+package ru.skillbranch.devintensive.extensions
+
+import ru.skillbranch.devintensive.models.User
+import ru.skillbranch.devintensive.models.UserView
+import ru.skillbranch.devintensive.utils.Utils
+import java.util.*
+
+fun User.toUserView(): UserView {
+
+    val nickName = Utils.transliteration("$firstName $lastName")
+    val initials = Utils.toInitials(firstName, lastName)
+    val status =
+        if (lastVisit == null) "Ещё ни разу не был"
+        else
+            if (isOnline) "online"
+            else "Последний раз был ${lastVisit.humanizeDiff()}"
+
+    return UserView(
+        id,
+        fullName = "$firstName $lastName",
+        nickName = nickName,
+        avatar = avatar,
+        status = status,
+        initials = initials
+    )
+}
+
+private fun Date.humanizeDiff(date: Date = Date()): String {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+}
