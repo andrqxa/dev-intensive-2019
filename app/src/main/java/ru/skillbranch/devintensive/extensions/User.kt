02b5a0c08ccdd2b4bebee87d19dae0ad6cf9a3c1
Andrii Puhachenko <andrqxa@gmail.com>
2019-07-08 02:37:40 +0300

    Added plurals

--- app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
@@ -5,6 +5,11 @@ import ru.skillbranch.devintensive.models.UserView
 import ru.skillbranch.devintensive.utils.Utils
 import java.util.*
 
+const val MINS = 60
+const val HOURS = 60 * MINS
+const val DAYS = 24 * HOURS
+
+
 fun User.toUserView(): UserView {
 
     val nickName = Utils.transliteration("$firstName $lastName")
@@ -14,7 +19,6 @@ fun User.toUserView(): UserView {
         else
             if (isOnline) "online"
             else "Последний раз был ${lastVisit.humanizeDiff()}"
-
     return UserView(
         id,
         fullName = "$firstName $lastName",
@@ -26,6 +30,20 @@ fun User.toUserView(): UserView {
 }
 
 fun Date.humanizeDiff(date: Date = Date()): String {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-
+    val current = Date().time
+    val previous = date.time
+    val diff = (current - previous) * SECOND
+    return when {
+        diff in 0..1 -> "только что"
+        diff in 2..45 -> "несколько секунд назад"
+        diff in 46..75 -> "минуту назад"
+//        diff in 76..45* MINS -> "N минут назад"
+        diff in 46 * MINS..75 * MINS -> "час назад"
+//        diff in 76*MINS..22* HOURS -> "N часов назад"
+        diff in 23 * HOURS..26 * HOURS -> "день назад"
+//        diff in 27*HOURS..360 * DAYS -> "N дней назад"
+        diff > (360 * DAYS) -> "более года назад"
+//        else -> IllegalArgumentException("Illegal time difference")
+        else -> "неизвестно"
+    }
 }
