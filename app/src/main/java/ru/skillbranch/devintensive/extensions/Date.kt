cf5cdfcd575a18c59ea2811381a4903dbfeeb733
Andrii Puhachenko <andrqxa@gmail.com>
2019-07-08 23:22:19 +0300

    Fix plural

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -36,61 +36,61 @@ enum class TimeUnits {
     HOUR,
     DAY,
     WEEK,
-    DECADE
-}
+    DECADE;
 
-fun TimeUnits.plural(value0: Int): String {
-    val suff = "$value0".last()
-    val value = abs(value0)
-    return when (this.name) {
-        "SECOND" -> {
-            when (suff) {
-                '0', '5', '6', '7', '8', '9' -> "$value секунд"
-                '1' -> "$value секунду"
-                '2', '3', '4' -> "$value секунды"
-                else -> ""
+    fun plural(value0: Int): String {
+        val suff = "$value0".last()
+        val value = abs(value0)
+        return when (this.name) {
+            "SECOND" -> {
+                when (suff) {
+                    '0', '5', '6', '7', '8', '9' -> "$value секунд"
+                    '1' -> "$value секунду"
+                    '2', '3', '4' -> "$value секунды"
+                    else -> ""
+                }
             }
-        }
-        "MINUTE" -> {
-            when (suff) {
-                '0', '5', '6', '7', '8', '9' -> "$value минут"
-                '1' -> "$value минуту"
-                '2', '3', '4' -> "$value минуты"
-                else -> ""
+            "MINUTE" -> {
+                when (suff) {
+                    '0', '5', '6', '7', '8', '9' -> "$value минут"
+                    '1' -> "$value минуту"
+                    '2', '3', '4' -> "$value минуты"
+                    else -> ""
+                }
             }
-        }
-        "HOUR" -> {
-            when (suff) {
-                '0', '5', '6', '7', '8', '9' -> "$value часов"
-                '1' -> "$value час"
-                '2', '3', '4' -> "$value часа"
-                else -> ""
+            "HOUR" -> {
+                when (suff) {
+                    '0', '5', '6', '7', '8', '9' -> "$value часов"
+                    '1' -> "$value час"
+                    '2', '3', '4' -> "$value часа"
+                    else -> ""
+                }
             }
-        }
-        "DAY" -> {
-            when (suff) {
-                '0', '5', '6', '7', '8', '9' -> "$value дней"
-                '1' -> "$value день"
-                '2', '3', '4' -> "$value дня"
-                else -> ""
+            "DAY" -> {
+                when (suff) {
+                    '0', '5', '6', '7', '8', '9' -> "$value дней"
+                    '1' -> "$value день"
+                    '2', '3', '4' -> "$value дня"
+                    else -> ""
+                }
             }
-        }
-        "WEEK" -> {
-            when (suff) {
-                '0', '5', '6', '7', '8', '9' -> "$value недель"
-                '1' -> "$value неделю"
-                '2', '3', '4' -> "$value недели"
-                else -> ""
+            "WEEK" -> {
+                when (suff) {
+                    '0', '5', '6', '7', '8', '9' -> "$value недель"
+                    '1' -> "$value неделю"
+                    '2', '3', '4' -> "$value недели"
+                    else -> ""
+                }
             }
-        }
-        "DECADE" -> {
-            when (suff) {
-                '0', '5', '6', '7', '8', '9' -> "$value декад"
-                '1' -> "$value декаду"
-                '2', '3', '4' -> "$value декад"
-                else -> ""
+            "DECADE" -> {
+                when (suff) {
+                    '0', '5', '6', '7', '8', '9' -> "$value декад"
+                    '1' -> "$value декаду"
+                    '2', '3', '4' -> "$value декад"
+                    else -> ""
+                }
             }
+            else -> ""
         }
-        else -> ""
     }
 }
