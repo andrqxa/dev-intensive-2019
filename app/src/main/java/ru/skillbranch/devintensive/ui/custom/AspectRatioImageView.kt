78ff368c8dc9b5500eba2cd4e2b577368b6978b8
Andrii Puhachenko <andrqxa@gmail.com>
2019-07-26 23:45:34 +0300

    Added CustomView into layout

--- app/src/main/java/ru/skillbranch/devintensive/ui/custom/AspectRatioImageView.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/custom/AspectRatioImageView.kt
@@ -3,16 +3,35 @@ package ru.skillbranch.devintensive.ui.custom
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.ImageView
+import ru.skillbranch.devintensive.R //Обязательно импортировать если надо рабоать с R
 
 class AspectRatioImageView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    fefStyleAttr: Int = 0
-) : ImageView(context, attrs, fefStyleAttr) {
+    defStyleAttr: Int = 0
+) : ImageView(context, attrs, defStyleAttr) {
     companion object {
         private const val DEFAULT_ASPECT_RATIO = 1.78f
     }
 
     private var aspectRatio = DEFAULT_ASPECT_RATIO
+
+    init {
+        if (attrs != null) {
+            val a = context.obtainStyledAttributes(attrs, R.styleable.AspectRatioImageView)
+            a.getFloat(
+                R.styleable.AspectRatioImageView_aspectRatio,
+                DEFAULT_ASPECT_RATIO
+            )  //берется из res/values/attrs
+            a.recycle() // ЖЕЛАТЕЛЬНО, НУЖНО ДЛЯ ОСВОБОЖДЕНИЯ РЕСУРСОВ
+
+        }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val newHeight = (measuredWidth / aspectRatio).toInt()
+        setMeasuredDimension(measuredWidth, newHeight)
+    }
 }
 
