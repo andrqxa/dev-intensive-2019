f33454436df1fad60766c1f9b8b2beb12faaa6ac
Andrii Puhachenko <andrqxa@gmail.com>
2019-07-28 23:28:37 +0300

    Added ProfileViewModel class

--- app/src/main/java/ru/skillbranch/devintensive/repositories/PreferencesRepository.kt
+++ app/src/main/java/ru/skillbranch/devintensive/repositories/PreferencesRepository.kt
@@ -1,3 +1,18 @@
 package ru.skillbranch.devintensive.repositories
 
-object PreferencesRepository
+import ru.skillbranch.devintensive.models.Profile
+
+object PreferencesRepository {
+    fun getProfileData(): Profile? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    fun saveProfile(profile: Profile?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    fun getProfile(): Profile? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
