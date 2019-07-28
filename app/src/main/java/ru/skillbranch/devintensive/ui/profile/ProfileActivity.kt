44fc43d5eb0ab92f948767f893913356d9cbc43e
Andrii Puhachenko <andrqxa@gmail.com>
2019-07-28 17:35:35 +0300

    Added saving state of editing

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -1,10 +1,25 @@
 package ru.skillbranch.devintensive.ui.profile
 
+import android.graphics.ColorFilter
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_profile_constraint.*
 import ru.skillbranch.devintensive.R
 
 class ProfileActivity : AppCompatActivity() {
+
+    companion object {
+        const val IS_EDIT_MODE = "IS_EDIT_MODE"
+    }
+
+    var isEditMode = false
+    lateinit var viewFields: Map<String, TextView>
+
     /**
      * Вызывается при первом создании или перезапуске Activity
      *
@@ -23,6 +38,79 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_profile)
         setContentView(R.layout.activity_profile_constraint)
+        initViews(savedInstanceState)
+    }
+
+    /**
+     * Этот метод сохраняет состояние представления в Bundle
+     * для API Level < 28(Android P) этот метод будет выполняться до onStop(), и нет никаких гарантий относительно того,
+     * произойдет ли это до или после onPause()
+     * Для API Level >= 28(Android P) будет вызван после onStop()
+     * Не будет вызван, если Activity будет явно закрыта пользователем при нажатии на системную клавишу Back
+     */
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putBoolean(IS_EDIT_MODE, isEditMode)
+    }
+
+    private fun initViews(savedInstanceState: Bundle?) {
+        viewFields = mapOf(
+            "nickname" to tv_nick_name,
+            "rank" to tv_rank,
+            "firstName" to et_first_name,
+            "lastName" to et_last_name,
+            "about" to et_about,
+            "repository" to et_repository,
+            "rating" to tv_rating,
+            "respect" to tv_respect
+            //"" to btn_edit,
+            //"" to btn_switch_theme,
+            //"" to iv_avatar,
+            //"" to wr_about,
+            //"" to wr_repository
+        )
+
+        isEditMode = savedInstanceState?.getBoolean(IS_EDIT_MODE, false) ?: false
+        showCurrentMode(isEditMode)
+
+        btn_edit.setOnClickListener {
+            isEditMode = !isEditMode
+            showCurrentMode(isEditMode)
+        }
+    }
+
+    private fun showCurrentMode(isEdit: Boolean) {
+//        val info = viewFields.filter { setOf("firstName", "lastName", "about", "repository").contains(it.key) }
+        val info = viewFields.filter { it.key in setOf("firstName", "lastName", "about", "repository") }
+        for ((_, v) in info) {
+            v as EditText
+            v.isFocusable = isEdit
+            v.isFocusableInTouchMode = isEdit
+            v.isEnabled = isEdit
+            v.background.alpha = if (isEdit) 255 else 0
+        }
+        ic_eye.visibility = if (isEdit) View.GONE else View.VISIBLE
+        wr_about.isCounterEnabled = isEdit
+
+        with(btn_edit) {
+            val filter: ColorFilter? = if (isEdit) {
+                PorterDuffColorFilter(
+                    resources.getColor(R.color.color_accent, theme),
+                    PorterDuff.Mode.SRC_IN
+                )
+            } else {
+                null
+            }
+
+            val icon = if (isEdit) {
+                resources.getDrawable(R.drawable.ic_save_black_24dp)
+            } else {
+                resources.getDrawable(R.drawable.ic_edit_black_24dp)
+            }
+
+            background.colorFilter = filter
+            setImageDrawable(icon)
+        }
     }
 
 }
