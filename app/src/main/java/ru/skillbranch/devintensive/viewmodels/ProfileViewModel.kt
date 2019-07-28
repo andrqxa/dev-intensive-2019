f945981633f473cd01efef0eb32ed9f9a47b1c80
Andrii Puhachenko <andrqxa@gmail.com>
2019-07-28 21:36:10 +0300

    Added ProfileViewModel

---
+++ app/src/main/java/ru/skillbranch/devintensive/viewmodels/ProfileViewModel.kt
@@ -0,0 +1,24 @@
+package ru.skillbranch.devintensive.viewmodels
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel : ViewModel() {
+
+    init {
+        Log.d("M_ProfileViewModel", "init view model")
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("M_ProfileViewModel", "view model cleared")
+    }
+
+    fun geProfileData() {
+
+    }
+
+    fun saveProfileData() {
+
+    }
+}
